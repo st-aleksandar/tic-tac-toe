282c8d070ec67ecec5b247572cc3f937f46262b7
st-aleksandar <aca_fpi@hotmail.com>
2019-11-24 12:15:19 +0100

    build

---
+++ precache-manifest.56a07c1fc6c8c334a88900ec8da1e23b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da136aaa9f551c30a6dca220efcf84ea",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "46ab3d6840c17317d1cc",
+    "url": "/tic-tac-toe/static/css/main.236ce676.chunk.css"
+  },
+  {
+    "revision": "0a99a5f0bf944c4ed20c",
+    "url": "/tic-tac-toe/static/js/2.f194a1c9.chunk.js"
+  },
+  {
+    "revision": "46ab3d6840c17317d1cc",
+    "url": "/tic-tac-toe/static/js/main.2606e263.chunk.js"
+  },
+  {
+    "revision": "5d593cda602e885327ad",
+    "url": "/tic-tac-toe/static/js/runtime-main.bfeff9e2.js"
+  },
+  {
+    "revision": "05d1bb2bd6670203a41c6a7406459c41",
+    "url": "/tic-tac-toe/static/media/o.05d1bb2b.svg"
+  },
+  {
+    "revision": "546dc5990e58f9254c2f730336e12a1e",
+    "url": "/tic-tac-toe/static/media/x.546dc599.svg"
+  }
+]);
